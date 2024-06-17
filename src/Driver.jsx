2e96be534bae14eea86673cf483ec101f8a615e4
clarkbarrett101 <clarkbarrett101@gmail.com>
2024-06-17 12:05:37 -0600

    a

--- src/Driver.jsx
+++ src/Driver.jsx
@@ -8,12 +8,14 @@ import LinkButton from "./LinkButton";
 
 function Driver({ setPage, kelvin, setKelvin }) {
   const min = 1800;
+  const [max, setMax] = useState(9000);
   const [flicker, setFlicker] = useState(false);
   const [luv, setLuv] = useState([0, 0, 0]);
   const [readings, setReadings] = useState([]);
   const [result, setResult] = useState(0);
   const [startCountdown, setStartCountdown] = useState(false);
   const [brightness, setBrightness] = useState(1);
+  const [kelvinModifier, setKelvinModifier] = useState(1000);
 
   useEffect(() => {
     (async () => {
@@ -49,6 +51,52 @@ function Driver({ setPage, kelvin, setKelvin }) {
     setReadings((prev) => [...prev, reading]);
     console.log(reading);
   }
+  function kelvinSequence() {
+    const diffIndexs = handleFindIndex();
+    const data = readings;
+    if (kelvinModifier > 100) {
+      setKelvinModifier(100);
+      console.log(data[diffIndexs[0]], data[diffIndexs[1]]);
+      if (diffIndexs[0] > diffIndexs[1]) {
+        setMax(data[diffIndexs[0]][2]);
+        return data[diffIndexs[1]][2];
+      } else {
+        setMax(data[diffIndexs[1]][2]);
+        return data[diffIndexs[0]][2];
+      }
+    } else {
+      handleCalculateCam();
+      ejectLoop();
+    }
+  }
+  function handleFindIndex() {
+    const data = readings;
+    let diff = 100;
+    let diffIndex1 = 0;
+    let diffIndex2 = 1;
+    let diffArray = [];
+    const control = data[0];
+    console.log(control);
+    for (let i = 1; i < data.length - 1; i += 1) {
+      let uDiff = Math.abs(data[i][0] - control[0]);
+      let vDiff = Math.abs(data[i][1] - control[1]);
+      let uvDiff = Math.sqrt(uDiff ** 2 + vDiff ** 2);
+      uvDiff = Math.round(uvDiff * 100) / 100;
+      console.log(data[i][2], uvDiff);
+      diffArray.push(uvDiff);
+    }
+    diff = diffArray[0];
+
+    for (let i = 1; i < diffArray.length; i++) {
+      if (diffArray[i] < diff) {
+        diff = diffArray[i];
+        diffIndex2 = diffIndex1;
+        diffIndex1 = i;
+      }
+    }
+    return [diffIndex1, diffIndex2];
+  }
+
   function handleCalculateCam() {
     const data = readings;
     let diff = 100;
@@ -64,19 +112,19 @@ function Driver({ setPage, kelvin, setKelvin }) {
       console.log(data[i][2], uvDiff);
       diffArray.push(uvDiff);
     }
-    let diffArrayNormal = [];
+    /* let diffArrayNormal = [];
     diffArrayNormal.push(diffArray[0]);
     for (let i = 1; i < diffArray.length - 1; i++) {
       diffArrayNormal.push((diffArray[i] + diffArray[i + 1]) / 2);
     }
     diffArrayNormal.push(diffArray[diffArray.length - 1]);
-    console.log(diffArrayNormal);
-    diffIndex = diffArrayNormal.length - 1;
-    diff = diffArrayNormal[diffArrayNormal.length - 1];
-    for (let i = diffArrayNormal.length - 2; i > 0; i--) {
-      console.log(diffArrayNormal[i], diff);
-      if (diffArrayNormal[i] < diff) {
-        diff = diffArrayNormal[i];
+    console.log(diffArrayNormal);*/
+    diffIndex = diffArray.length - 1;
+    diff = diffArray[diffArray.length - 1];
+    for (let i = diffArray.length - 2; i > 0; i--) {
+      console.log(diffArray[i], diff);
+      if (diffArray[i] < diff) {
+        diff = diffArray[i];
         diffIndex = i;
       }
     }
@@ -99,11 +147,14 @@ function Driver({ setPage, kelvin, setKelvin }) {
     if (flicker) {
       const interval = setInterval(() => {
         setKelvin((prev) => {
-          if (prev < 9000) {
-            return prev + 200;
+          if (prev < max) {
+            if (prev === min) {
+              return prev + 200;
+            } else {
+              return prev + kelvinModifier;
+            }
           } else {
-            clearInterval(interval);
-            return 9000;
+            return kelvinSequence();
           }
         });
       }, 100);
@@ -116,14 +167,13 @@ function Driver({ setPage, kelvin, setKelvin }) {
   useEffect(() => {
     if (flicker) {
       handleTakeReading();
-      if (kelvin >= 9000) {
-        setFlicker(false);
-        setKelvin(min);
-        handleCalculateCam();
-        setReadings([]);
-      }
     }
   }, [kelvin, flicker]);
+  function ejectLoop() {
+    setFlicker(false);
+    setKelvin(min);
+    setReadings([]);
+  }
 
   function explainer() {
     return (
